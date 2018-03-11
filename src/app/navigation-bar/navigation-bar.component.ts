8d9e3284d7482394f5ee1e69ec5d109e20319e3d
Sitdhibong Laokok <sitdhibong@gmail.com>
2018-03-11 19:04:51 +0700

    Update navigation bar

--- src/app/navigation-bar/navigation-bar.component.ts
+++ src/app/navigation-bar/navigation-bar.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, Input } from '@angular/core';
 
 import { MenuItemService } from '../menu-item.service';
+import { UserInformationService } from '../user-information.service';
+import { UserInformation } from '../user-information.entity';
 
 @Component({
   selector: 'app-navigation-bar',
@@ -11,23 +13,38 @@ export class NavigationBarComponent implements OnInit {
 
   @Input('appTitle') public title: string
 
-  public isAuthenticated = true
+  public isAuthenticated = false
 
   public docorator = 'none'
 
   private items = []
 
+  private user: UserInformation
+
   constructor(
-    private menuItemService: MenuItemService
+    private menuItemService: MenuItemService,
+    private userInformation: UserInformationService,
   ) { }
 
   ngOnInit() {
     this.retrieveRegisteredItems()
+    this.authenticateCheck()
   }
 
   retrieveRegisteredItems() {
     this.menuItemService.menuItems()
       .subscribe( items => this.items = items )
+  }
+
+  authenticateCheck() {
+    this.userInformation.isAuthenticate()
+      .subscribe( user => { 
+        if (null == user) {
+          this.isAuthenticate = false
+        } else {
+          this.userInfo = user;
+        }
+      })
   }
 
 }
