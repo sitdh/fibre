f846e6ba41758562919165e274fc2914c30ce44e
Sitdhibong Laokok <sitdhibong@gmail.com>
2018-03-12 14:39:38 +0700

    Add create new user account

--- src/app/create-user-account/create-user-account.component.ts
+++ src/app/create-user-account/create-user-account.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateUserAccountComponent implements OnInit {
 
+  pageTitle = 'New user'
+
   constructor() { }
 
   ngOnInit() {
