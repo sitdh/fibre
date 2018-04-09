b0beea3ebf2ed994f988b9fb45077e7c07783097
Sitdhibong Laokok <sitdhibong@gmail.com>
2018-04-09 12:01:17 +0700

    Add repository fetch service

---
+++ src/app/repository.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { AngularfireAuth } from 'angularfire2/auth';
+
+import { Observable } from 'rxjs/Observable';
+import { AuthenGuardService } from './authen-guard.service';
+import { environment } from './../../environments/environment';
+
+import { 
+
+@Injectable()
+export class RepositoryService {
+
+  constructor(
+    ag: AuthenGuardService
+  ) { 
+
+  }
+
+}
