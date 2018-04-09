a87f86954ba122eb51743393cde7163fd67c3b8d
Sitdhibong Laokok <sitdhibong@gmail.com>
2018-04-09 15:13:54 +0700

    Add repo fetch subscription

--- src/app/repository.service.ts
+++ src/app/repository.service.ts
@@ -1,19 +1,23 @@
 import { Injectable } from '@angular/core';
-import { AngularfireAuth } from 'angularfire2/auth';
+import { HttpClient } from '@angular/common/http';
+import { AngularFireAuth } from 'angularfire2/auth';
 
 import { Observable } from 'rxjs/Observable';
-import { AuthenGuardService } from './authen-guard.service';
-import { environment } from './../../environments/environment';
+import { of } from 'rxjs/observable/of';
 
-import { 
+import { AuthenGuardService } from './authen-guard.service';
+import { environment as env } from './../environments/environment';
 
 @Injectable()
 export class RepositoryService {
 
   constructor(
-    ag: AuthenGuardService
-  ) { 
+    private ag: AuthenGuardService,
+    private http: HttpClient
+  ) { }
 
+  fetchRepositories(u: string): Observable<any[]> {
+    return this.http.get(`https://api.github.com/users/${u}/repos`);
   }
 
 }
