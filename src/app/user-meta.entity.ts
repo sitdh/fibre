989f95dc25edc77446c7276dc5e9f5deef3e008e
Sitdhibong Laokok <sitdhibong@gmail.com>
2018-04-09 17:54:47 +0700

    Create user meta interface

---
+++ src/app/user-meta.entity.ts
@@ -0,0 +1,11 @@
+export interface UserMeta {
+  username?: string;
+  profile_url?: string;
+  provider?: string;
+  display_name?: string;
+  avatar_url?: string;
+  repos_url?: string;
+  url?: string;
+  bio?: string;
+  blog?: string;
+}
