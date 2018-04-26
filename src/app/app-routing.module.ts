ea9390c22797674bcb82e7bce6ae6f37eb60819d
Sitdhibong Laokok <sitdhibong@gmail.com>
2018-04-26 19:23:34 +0700

    Update routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,12 +7,14 @@ import { HomeComponent } from './home/home.component';
 import { QuiteComponent } from './quite/quite.component';
 import { SettingsComponent } from './settings/settings.component';
 import { CreateUserAccountComponent } from './create-user-account/create-user-account.component';
+import { ProjectManagementComponent } from './project-management/project-management.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
   { path: 'dashboard', component: DashboardComponent },
   { path: 'project/new', component: CreateProjectComponent },
+  { path: 'p/:pid', component: ProjectManagementComponent },
   { path: 'settings', component: SettingsComponent },
   { path: 'quite', component: QuiteComponent },
   { path: 'profile/:username', component: HomeComponent },
