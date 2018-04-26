fbfd1522ad1ffb213da0712f7df34d0cde160157
Sitdhibong Laokok <sitdhibong@gmail.com>
2018-04-26 19:24:19 +0700

    Refine form

--- src/app/create-project/create-project.component.ts
+++ src/app/create-project/create-project.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { HttpClient, HttpParams } from '@angular/common/http';
+import { Router } from '@angular/router';
 
 import { AngularFireAuth } from 'angularfire2/auth';
 import { AngularFirestore, AngularFirestoreDocument } from 'angularfire2/firestore';
@@ -21,13 +22,16 @@ export class CreateProjectComponent implements OnInit {
 
   selectedRepo: {};
 
+  user: any;
+
   constructor(
     private _formBuilder: FormBuilder,
+    private route: Router,
     private afa: AngularFireAuth,
     private afs: AngularFirestore,
     private repoService: RepositoryService,
     private http: HttpClient,
-    private ag: AuthenGuardService
+    private ag: AuthenGuardService,
   ) { 
     this.ag.currentObservedUser().subscribe(u => {
       this.fetchUserRepositories(u)
@@ -43,17 +47,40 @@ export class CreateProjectComponent implements OnInit {
       .valueChanges().subscribe(repos => {
         this.repositories = repos
       })
+
+    this.user = user;
   }
 
   cancelProjectSubmit() {
   }
 
   createNewProject(projectInfo) {
-    console.log(projectInfo)
+    var repo = this.selectedRepo[0]
+    var projectAlias = projectInfo.name.toLowerCase().replace(' ', '-')
+    var projectName = (this.user.uid + " " + projectAlias)
+      .toLowerCase()
+      .replace(' ', '-')
+
+    var data = {
+      name: projectInfo.name,
+      language: repo.language,
+      description: projectInfo.describe,
+      repo: repo.git_url,
+      htmlurl: repo.html_url,
+      project_location: '/p/' + projectAlias,
+    }
+
+    this.afs.collection('/projects')
+      .doc(this.user.uid)
+      .collection('/repo')
+      .doc(projectName)
+      .set(data)
+      .then(() => {
+        this.route.navigate([data.project_location])
+    })
   }
 
   selectedRepositoryChanged(event: any) {
-    console.log(event)
-    // let repo = this.repositories.filter(e => e.id == event.value)
+    this.selectedRepo = this.repositories.filter(e => e.id == event.value)
   }
 }
