c46d984e432937fed5b40031543b4d38f15bd7ec
Sitdhibong Laokok <sitdhibong@gmail.com>
2018-04-28 14:25:50 +0700

    Update project query

--- src/app/project-management/project-management.component.ts
+++ src/app/project-management/project-management.component.ts
@@ -7,6 +7,10 @@ import { AngularFireAuth } from 'angularfire2/auth';
 import { AngularFirestore, AngularFirestoreDocument } from 'angularfire2/firestore';
 import * as firebase from 'firebase/app';
 
+import { AuthenGuardService } from '../authen-guard.service';
+import { Project } from '../project.entity';
+import { UserMeta } from '../user-meta.entity';
+
 @Component({
   selector: 'app-project-management',
   templateUrl: './project-management.component.html',
@@ -14,20 +18,35 @@ import * as firebase from 'firebase/app';
 })
 export class ProjectManagementComponent implements OnInit {
 
+  project: Project
+
+  projectTitle: string
+
+  currentUser: firebase.UserInfo 
+
   constructor(
     private route: ActivatedRoute,
     private af: AngularFireAuth,
-    private afs: AngularFirestore
+    private afs: AngularFirestore,
+    private ag: AuthenGuardService
   ) { 
-
-    var ma = afs.collection<any>('/projects')
-    ma.valueChanges().subscribe(k => {
-      console.log(k)
+    this.ag.currentObservedUser().subscribe(u => {
+      this.currentUser = u
+      this.fetchingProjectInformation(u)
     })
-
   }
 
   ngOnInit() {
+  }
+
+  fetchingProjectInformation(user) {
+    var q = this.afs.collection('projects', ref => {
+      return ref.where('slug', '==', 'hello-world').where('owner', '==', user.uid)
+    })
+    console.log(q)
+    q.valueChanges().subscribe(p => {
+      this.project = p.pop()
+    })
   }
 
 }
