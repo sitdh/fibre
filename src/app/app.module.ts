f769699173579813114423fab27b752bd43c071c
Sitdhibong Laokok <sitdhibong@gmail.com>
2018-05-14 16:03:51 +0700

    Restructure imported dialog component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -89,6 +89,7 @@ import { ProjectCreationProgressDialogComponent } from './project-creation-progr
   ],
   entryComponents: [
     JenkinsSettingsDialogComponent,
+    ProjectCreationProgressDialogComponent,
   ],
   providers: [
     MenuItemService,
